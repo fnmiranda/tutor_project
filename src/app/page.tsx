e6474860178ea7388207996f8d12428901c98c62
Domingos <domingosthiago.td@gmail.com>
2025-10-19 13:21:43 -0300

    Deixando a page no padrao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import ProfessorDashboard from "../pages/dashboard/dashboard";
-import PageProfessor from "@/pages/perfil_professor/perfil_professor";
-import PagePerfilAluno from "@/pages/perfil_aluno/perfil_aluno";
 export default function Home() {
   return (
     <div className="font-sans ">
       <main className="flex flex-col">
         <ProfessorDashboard />
-        <PageProfessor />
-        <PagePerfilAluno />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
