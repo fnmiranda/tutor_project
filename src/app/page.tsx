5d32403fdfd13d1902bb4c167258b6fa26c2f888
Domingos <domingosthiago.td@gmail.com>
2025-10-19 13:05:54 -0300

    Atualização das paginas perfis, alem da adicao das configuracoes de preferencias e notificacoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import ProfessorDashboard from '../pages/dashboard/dashboard';
-
+import ProfessorDashboard from "../pages/dashboard/dashboard";
+import PageProfessor from "@/pages/perfil_professor/perfil_professor";
+import PagePerfilAluno from "@/pages/perfil_aluno/perfil_aluno";
 export default function Home() {
   return (
     <div className="font-sans ">
       <main className="flex flex-col">
-        <ProfessorDashboard/>
- 
+        <ProfessorDashboard />
+        <PageProfessor />
+        <PagePerfilAluno />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
